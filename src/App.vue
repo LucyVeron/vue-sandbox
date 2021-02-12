3bd6cd3dd5af4c7a7794743464516ee134dc77a1
Lucy Veron <lucymveron@gmail.com>
2021-02-12 13:32:35 +0100

    learning directives, methods, variables

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Store</h1>
+  <div v-for="item in items" v-bind:key="item">
+    <input v-model="item.stock">
+    <span>{{item.name}}</span>
+    <button v-on:click="add(item.name)">+</button>
+    <button v-on:click="remove(item.name)">-</button>
+  </div>
+  <br>
+  <span>Total inventory: {{ total }}</span>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [
+        {name: 'Compass', stock: 0},
+        {name: 'Jacket', stock: 0},
+        {name: 'Hiking Socks', stock: 0},
+        {name: 'Suntan Lotion', stock: 0}
+      ]
+    }
+  },
+  computed() {
+    return {
+      total () {
+        this.items.reduce((sum, product) => {
+          return sum + product.stock
+        })
+      }
+    }
+  },
+  methods: {
+    add(name) {
+      this.items.find((item) => item.name === name).stock++;
+    },
+    remove(name) {
+      this.items.find((item) => item.name === name).stock--;
+    }
   }
 }
 </script>
